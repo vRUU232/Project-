ed21c072f8f4874414deafccc6e873a879974dd0
patelprit21 <80966293+patelprit21@users.noreply.github.com>
2024-08-08 01:10:25 -0400

    Define struct for a node in the Binary Search Tree

--- project.cpp
+++ project.cpp
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef struct Parcel
 {
     char* destination;
     int weight;
     float value;
 } Parcel;
+
+typedef struct BSTNode
+{
+    Parcel* parcel;    
+    struct BSTNode* left;    
+    struct BSTNode* right;    
+} BSTNode;
+
+typedef struct BST 
+{
+    BSTNode* root;     
+} BST;
